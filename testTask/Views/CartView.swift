c192e784f442df43d566e76eb70889136a16fd76
alirast <xdabnormal@gmail.com>
2023-07-02 00:55:57 +0300

    WIP: CartView template done

--- testTask/Views/CartView.swift
+++ testTask/Views/CartView.swift
@@ -14,65 +14,64 @@ struct CartView: View {
     @State private var number = 1
     
     var body: some View {
-        
         VStack {
-            Spacer()
-            HStack {
-                
-                VStack(alignment: .leading) {
-                    HStack {
-                        Spacer().frame(width: 62, height: 62).background(Color(red: CGFloat(248.0/255), green: CGFloat(247.0/255), blue: CGFloat(245.0/255))).cornerRadius(10).overlay {
-                            Image(systemName: "person")
-                        }
-                        VStack(alignment: .leading) {
-                            Text("Salad").font(.custom("SFProDisplay", size: 14))
-                            HStack(spacing: 3) {
-                                Text("Price₽").font(.custom("SFProDisplay", size: 14))
-                                Text("•").foregroundColor(.gray)
-                                Text("Weight").font(.custom("SFProDisplay", size: 14)).foregroundColor(.gray)
-                            }
-                            
-                        }
-                    }
-                }.frame(width: 250, alignment: .leading)
-                
-                VStack(alignment: .trailing) {
-                    HStack {
+            ScrollView {
+                ForEach(0...5, id: \.self) { item in
+                    VStack {
                         Spacer()
-                            .frame(width: 99, height: 32).background(Color(red: CGFloat(248.0/255), green: CGFloat(247.0/255), blue: CGFloat(245.0/255))).cornerRadius(10).overlay {
-                                HStack(spacing: 20) {
-                                    Button(action: {
-                                        if number > 0 {
-                                            number -= 1
-                                        }
-                                    }) {
-                                        Image(systemName: "minus").foregroundColor(.black)
+                        HStack {
+                            
+                            VStack(alignment: .leading) {
+                                HStack(spacing: 3) {
+                                    Spacer().frame(width: 62, height: 62).background(Color(red: CGFloat(248.0/255), green: CGFloat(247.0/255), blue: CGFloat(245.0/255))).cornerRadius(10).overlay {
+                                        Image(systemName: "person")
                                     }
-                                    Text("\(number)")
-                                    
-                                    Button(action: {
-                                        number += 1
-                                    }) {
-                                        Image(systemName: "plus").foregroundColor(.black)
+                                    VStack(alignment: .leading) {
+                                        Text("Salad").font(.custom("SFProDisplay", size: 14))
+                                        HStack(spacing: 3) {
+                                            Text("Price₽").font(.custom("SFProDisplay", size: 14))
+                                            Text("•").foregroundColor(.gray)
+                                            Text("Weight").font(.custom("SFProDisplay", size: 14)).foregroundColor(.gray)
+                                        }
+                                        
                                     }
                                 }
-                            }
+                            }.frame(width: 250, alignment: .leading)
+                            
+                            
+                            VStack(alignment: .trailing) {
+                                HStack {
+                                    Spacer()
+                                        .frame(width: 99, height: 32).background(Color(red: CGFloat(248.0/255), green: CGFloat(247.0/255), blue: CGFloat(245.0/255))).cornerRadius(10).overlay {
+                                            HStack(spacing: 20) {
+                                                Button(action: {
+                                                    if number > 0 {
+                                                        number -= 1
+                                                    }
+                                                }) {
+                                                    Image(systemName: "minus").foregroundColor(.black)
+                                                }
+                                                Text("\(number)")
+                                                
+                                                Button(action: {
+                                                    number += 1
+                                                }) {
+                                                    Image(systemName: "plus").foregroundColor(.black)
+                                                }
+                                            }
+                                        }
+                                }
+                            }.frame(width: 100, alignment: .trailing)
+                        }
+                        Spacer()
                     }
-                }.frame(width: 100, alignment: .trailing)
+                }
+                
+                
+                
+                
             }
             Spacer()
-            
-            List {
-                ForEach(0...5, id: \.self) { treat in
-                    Text("treat")
-                }
-                //.onDelete(perform: order.deleteItems)
-            }
-            .listStyle(.plain)
-            
-            //if order.items.isEmpty {
-                Text("you have nothing to pay for")
-            //}
             
             Button {
                 print("Оплатить")
@@ -84,11 +83,9 @@ struct CartView: View {
                     .frame(width: 360, height: 44)
                     .background(Color(.systemBlue))
                     .cornerRadius(10)
-            }.padding(.bottom)
-            
-
+            }
+            .padding()
         }
-
         
         .toolbar {
             ToolContent()
